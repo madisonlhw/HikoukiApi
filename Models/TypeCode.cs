ffc9a873a8dd4ff19c5ecbec67ab716870e1a346
Madison Wass <madison@ccerailway.com>
2026-05-28 22:25:53 -0400

    TypeCode and TypeCodeVariant models

---
+++ Models/TypeCode.cs
@@ -0,0 +1,10 @@
+﻿namespace HikoukiApi.Models
+{
+    public class TypeCode
+    {
+        public int Id { get; set; }
+        public required string Icao { get; set; }
+        public required string Manufacturer { get; set; }
+        public ICollection<TypeCodeVariant> ModelVariants { get; set; } = [];
+    }
+}
---
+++ Models/TypeCodeVariant.cs
@@ -0,0 +1,9 @@
+﻿namespace HikoukiApi.Models
+{
+    public class TypeCodeVariant
+    {
+        public int Id { get; set; }
+        public required int TypeCodeId { get; set; }
+        public required string VariantName { get; set; }
+    }
+}
